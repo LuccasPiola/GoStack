6f1fd96a6be25c8ce8b598f6f01ad9cfe2d3d638
Luccas Piola <luccaspiola@MacBook-Pro-de-Luccas.local>
2020-09-24 14:41:21 -0300

    feat: Add cors

--- project/backend/src/server.ts
+++ project/backend/src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata'
 import express from 'express'
+import cors from 'cors'
 import 'express-async-errors'
 import routes from './routes'
 import './database'
@@ -8,6 +9,7 @@ import exceptionHandle from './middlewares/exceptionHandle'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/files', express.static(uploadConfig.directory))
 app.use(routes)
